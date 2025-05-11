c215d1c6a38621e211550bc0ba066b13910db5c5
Rahul Boga <159926174+rahul-boga-19@users.noreply.github.com>
2025-05-11 21:45:28 +0530

    Update wsgi.py

--- coderhub/coderhub/wsgi.py
+++ coderhub/coderhub/wsgi.py
@@ -6,11 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
-
+# coderhub/coderhub/wsgi.py
 import os
-
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coderhub.settings')
-
 application = get_wsgi_application()

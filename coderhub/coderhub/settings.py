9fef9528a21f7c589e893b941ef987a249bde6ec
Rahul Boga <159926174+rahul-boga-19@users.noreply.github.com>
2025-05-11 21:38:21 +0530

    Update settings.py

--- coderhub/coderhub/settings.py
+++ coderhub/coderhub/settings.py
@@ -84,34 +84,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'coderhub.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# import os
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.environ.get('DB_NAME','ch'),  # Using 'ch' as a fallback if not set
-#         'USER': os.environ.get('DB_USER'),  # Default user if not set
-#         'PASSWORD': os.environ.get('DB_PASSWORD'),  # Default password if not set
-#         'HOST': os.environ.get('DB_HOST'),  # Default host if not set
-#         'PORT': os.environ.get('DB_PORT', '3306'),  # Default port if not set
-   
-
-#   'OPTIONS': {  # Add this section
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         }
-#     }
-
-# }
 
 # settings.py
 import os

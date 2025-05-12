a3d2032001bec7d107ba0b44d9098ee01f877646
Rahul Boga <159926174+rahul-boga-19@users.noreply.github.com>
2025-05-12 20:44:59 +0530

    Update settings.py

--- coderhub/coderhub/settings.py
+++ coderhub/coderhub/settings.py
@@ -203,3 +203,11 @@ import dj_database_url
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True  # Enforce SSL
+    )
+}
